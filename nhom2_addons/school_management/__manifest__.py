990a7e4b3d36f95992565c42b3a697ca3d0d3d5c
NguyenThiMan44K212 <77474759+NguyenThiMan44K212@users.noreply.github.com>
2021-10-20 12:31:40 +0700

    Up Module

---
+++ nhom2_addons/school_management/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'School Management',
+    'version': '1.0',
+    'summary': 'School Management Software',
+    'sequence': -100,
+    'description': """School Management Software""",
+    'category': 'Productivity',
+    'website': 'https://www.odoo.school',
+    'license': 'LGPL-3',
+    'depends': [],
+    'data': [
+
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
