00bab37589f37e746b15610d47a460222456eeab
myduyen2000 <78034720+myduyen2000@users.noreply.github.com>
2021-10-23 01:20:04 +0700

    Quản lý học phần

--- nhom2_addons/school_management/models/hocphan.py
+++ nhom2_addons/school_management/models/hocphan.py
@@ -1,26 +1,27 @@
 from odoo import api, fields, models, tools, _
 from odoo.exceptions import UserError, ValidationError
-class Term(models.Model):
-    _name = "my.term"
-    _description = "Term model"
-    kyhoc = fields.Char('Ky hoc:', required=True)
-    tenhp = fields.Char('Ten HP:', required=True)
-    mahp = fields.Char('Ma HP:', required=True)
-    sotc = fields.Integer('So tin chi:', required=True)
-    loaihp = fields.Selection([
-        ('chuyennganh', 'Chuyen nganh'),
-        ('binhthuong', 'Binh thuong'),
-        ('khac', 'Khac')
-    ], string='Loai HP:', default='Chuyen nganh')
-    sotlt = fields.Integer('So lop ly thuyet:', required=True)
-    sotth = fields.Integer('So lop thuc hanh:', required=True)
-    ghichu = fields.Selection([
-        ('moi', 'Hoc moi'),
-        ('lai', 'Hoc lai'),
-        ('caithien', 'Cai thien')
-    ], string='Ghi chu:', default='Hoc moi')
-    tgbd =fields.Date('Thoi gian bat dau:', required=False)
-    tgkt = fields.Date('Thoi gian ket thuc:', required=False)
-    lich = fields.Char('Lich hoc:', required=True)
-    phong = fields.Char('Phong:', required=True)
-    ngaythi = fields.Date('Ngay thi:', required=False)
+class course(models.Model):
+    _name = "my.course"
+    _description = "course model"
+    semester = fields.Char('semester :', required=True)
+    coursename = fields.Char('course name :', required=True)
+    coursecode = fields.Char('course code :', required=True)
+    credits = fields.Integer('number of credits :', required=True)
+    coursetype  = fields.Selection([
+        ('major', 'major'),
+        ('normal', 'normal'),
+        ('others', 'others')
+    ], string='Course Type:', default='major')
+    theoretical = fields.Integer('number of theoretical periods :', required=True)
+    practice = fields.Integer('number of practice periods :', required=True)
+    note = fields.Selection([
+        ('learnnew', 'learn new'),
+        ('learnimprovement', 'learn improvement'),
+        ('learnagain', 'learn again')
+    ], string='Note:', default='learn new')
+    total = fields.Integer('total:', required=True)
+    starttime =fields.Date('start time:', required=False)
+    endtime = fields.Date('end time:', required=False)
+    schedule = fields.Char('schedule:', required=True)
+    room = fields.Char('room:', required=True)
+    examday = fields.Date('exam day:', required=False)
