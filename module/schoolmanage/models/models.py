55183d4bad8746055c6595c3bc48831e818a778f
HoTanHuy <78059369+HoTanHuy@users.noreply.github.com>
2021-10-29 19:57:13 +0700

    huy edit
    huy

--- module/schoolmanage/models/models.py
+++ module/schoolmanage/models/models.py
@@ -8,12 +8,12 @@ class Department(models.Model):
     _description = 'Department.schoolmanage'
     _inherit = ['mail.thread', 'mail.activity.mixin', 'portal.mixin']
 
-    Department_name = fields.Char("Tên khoa", required=True)
-    Department_code = fields.Char("Mã khoa", required=True)
-    Dean_name = fields.Char("Tên trưởng Khoa", required=True)
-    phone = fields.Integer("Số điện thoại")
+    Department_name = fields.Char("Department_name", required=True)
+    Department_code = fields.Char("Department_code", required=True)
+    Dean_name = fields.Char("Dean_name", required=True)
+    phone = fields.Integer("phone")
     gmail = fields.Char("Gmail", required=True)
-    address = fields.Char("Địa chỉ", required=True)
-    specialized_name = fields.Char("Tên Ngành", required=True)
-    specialized_code = fields.Char("Mã Ngành", required=True)
+    address = fields.Char("address", required=True)
+    specialized_name = fields.Char("specialized_name", required=True)
+    specialized_code = fields.Char("specialized_code", required=True)
 
