f2f7d17655faff333b9b14e7cd6b20120ecf90de
NguyenThiMan44K212 <77474759+NguyenThiMan44K212@users.noreply.github.com>
2021-10-31 15:06:30 +0700

    UP NEW

---
+++ Nhom2_THERP/school/models/department.py
@@ -0,0 +1,24 @@
+import datetime
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class Department(models.Model):
+    _inherit = "hr.department"
+    _description = "Department"
+
+    department_id = fields.Char(string='KH', required=True, copy=False, readonly=True,
+                              default=lambda seft: _('New'))
+    ology_list = fields.Many2many('school.ology', string='Ology List', required=True)
+    dean = fields.Many2many('hr.employee', string='Leader', required=True)
+    foundation_date = fields.Date('Foundation date', required=True)
+    phone = fields.Char("Contact Phone", required=True)
+    email = fields.Char("Contact Email", required=True)
+
+    @api.model
+    def create(self, vals):
+        if vals.get('department_id', ('New')) == ('New'):
+            vals['department_id'] = self.env['ir.sequence'].next_by_code('hr.department') or _('New')
+        res = super(Department, self).create(vals)
+        return res
